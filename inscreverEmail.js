130b5ccc0258300cbc3d3f8c433599efa1f50d1a
TalitaFavore <talitafavore@gmail.com>
2024-04-22 17:32:36 -0300

    Validando e-mail stay loop da contact us

--- inscreverEmail.js
+++ inscreverEmail.js
@@ -1,11 +1,22 @@
+function validarEmail(email) {
+  const padraoEmail = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return padraoEmail.test(email);
+}
+
+
 function email1() {
-    const email1 = document.getElementById("email1");
+    const email1 = document.getElementById("email1").value.trim();
   
     let dados = JSON.parse(localStorage.getItem("dadosEmail1"));
   
     if (dados == null) {
       localStorage.setItem("dadosEmail1", "[]");
       dados = [];
+    }
+
+    if (!validarEmail(email1)) {
+      document.getElementById('erroEmail1').innerText = 'Email inválido.';
+      return;
     }
   
     let auxRegistro = {
