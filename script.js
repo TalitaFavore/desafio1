0a085a1fc1733450830e057bcce98ae1e23bfe2a
TalitaFavore <talitafavore@gmail.com>
2024-04-21 21:41:58 -0300

    Criando localStorage para campos de email do Stay in the Loop

--- script.js
+++ script.js
@@ -29,3 +29,49 @@ function newsletter() {
   emailForm.value = "";
   message.value = "";
 }
+
+function email1() {
+  const email1 = document.getElementById("email1");
+
+  let dados = JSON.parse(localStorage.getItem("dadosEmail1"));
+
+  if (dados == null) {
+    localStorage.setItem("dadosEmail1", "[]");
+    dados = [];
+  }
+
+  let auxRegistro = {
+    email1: email1.value
+  }
+
+  dados.push(auxRegistro);
+  console.log(auxRegistro);
+
+  localStorage.setItem("dadosEmail1", JSON.stringify(dados));
+  alert("Registro incluido com sucesso!")
+
+  email1.value = "";
+}
+
+function email2() {
+  const email2 = document.getElementById("email2");
+
+  let dados = JSON.parse(localStorage.getItem("dadosEmail2"));
+
+  if (dados == null) {
+    localStorage.setItem("dadosEmail2", "[]");
+    dados = [];
+  }
+
+  let auxRegistro = {
+    email2: email2.value
+  }
+
+  dados.push(auxRegistro);
+  console.log(auxRegistro);
+
+  localStorage.setItem("dadosEmail2", JSON.stringify(dados));
+  alert("Registro incluido com sucesso!")
+
+  email2.value = "";
+}
