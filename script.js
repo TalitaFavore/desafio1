d6f65acfe9d79253c40775bf5d9c82fae67e23f0
TalitaFavore <talitafavore@gmail.com>
2024-04-22 15:41:59 -0300

    Fazendo validações do form com regex

--- script.js
+++ script.js
@@ -75,3 +75,84 @@ function email2() {
 
   email2.value = "";
 }
+
+function validarFirstName(firstName) {
+  const padraoNome = /^[a-zA-Z\s]*$/;
+  return padraoNome.test(firstName);
+}
+
+function validarLastName(lastName) {
+  const padraoSobrenome = /^[a-zA-Z\s]*$/;
+  return padraoSobrenome.test(lastName);
+}
+
+function validarEmail(email) {
+  const padraoEmail = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return padraoEmail.test(email);
+}
+
+function validarMessage(message) {
+  return message.trim().length >= 10;
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.querySelector('form');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    validarFormulario();
+  });
+
+function validarFormulario() {
+  const firstNameInput = document.getElementById('firstName').value.trim();
+  const lastNameInput = document.getElementById('lastName').value.trim();
+  const emailInput = document.getElementById('emailForm').value.trim();
+  const messageInput = document.getElementById('message').value.trim();
+
+  if (firstNameInput === '') {
+    document.getElementById('erroFirstName').innerText = 'Por favor, preencha o campo.';
+    return;
+  } else if (!validarFirstName(firstNameInput)) {
+    document.getElementById('erroFirstName').innerText = 'Nome inválido';
+    return;
+  } else {
+    document.getElementById('erroFirstName').innerText = '';
+  }
+  if (lastNameInput === '') {
+    document.getElementById('erroLastName').innerText = 'Por favor, preencha o campo.';
+    return;
+  } else if (!validarLastName(lastNameInput)) {
+    document.getElementById('erroLastName').innerText = 'Sobrenome inválido.';
+    return;
+  } else {
+    document.getElementById('erroLastName').innerText = '';
+  }
+  if (emailInput === '') {
+    document.getElementById('erroEmail').innerText = 'Por favor, preencha o campo.';
+    return;
+  } else if (!validarEmail(emailInput)) {
+    document.getElementById('erroEmail').innerText = 'Email inválido.';
+    return;
+  } else {
+    document.getElementById('erroEmail').innerText = '';
+  }
+  if (messageInput === '') {
+    document.getElementById('erroMessage').innerText = 'Por favor, preencha o campo.';
+    return;
+  } else if (!validarMessage(messageInput)) {
+    document.getElementById('erroMessage').innerText = 'Mensagem inválida. Escreva pelo menos uma frase com 10 caracteres.';
+    return;
+  } else {
+    document.getElementById('erroMessage').innerText = '';
+  }
+
+  newsletter();
+}
+});
+
+
+
+
+
